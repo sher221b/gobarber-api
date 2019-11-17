3e9779f8b8d0a2534b6f01b8ce229283b7af91de
sher221b <claudiojrs1993@gmail.com>
2019-11-17 17:11:03 -0300

    Test rota netilify

--- src/routes.js
+++ src/routes.js
@@ -19,8 +19,6 @@ const upload = multer(multerConfig);
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
 
-routes.get('/', (req, res) => res.send('ok =)'));
-
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
